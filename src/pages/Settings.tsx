efd64c2a3f6c61c8830daf0342e1766178126f91
sangukO <woqls1972@gmail.com>
2025-10-13 14:37:59 +0900

    feat: indexedDB 초기화 기능 추가

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,5 +1,31 @@
+import { db } from "@/lib/db";
+
+async function handleResetAllData() {
+  if (
+    !window.confirm(
+      `정말로 모든 데이터를 삭제하시겠습니까?
+이 작업은 되돌릴 수 없습니다.`
+    )
+  ) {
+    return;
+  }
+
+  try {
+    await db.sales.clear();
+    console.log("모든 데이터가 초기화되었습니다.");
+  } catch (error) {
+    console.error("데이터 초기화 실패:", error);
+  }
+}
+
 function Settings() {
-  return <div>Settings Page</div>;
+  return (
+    <div>
+      <ul>
+        <li onClick={handleResetAllData}>데이터 초기화</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Settings;
